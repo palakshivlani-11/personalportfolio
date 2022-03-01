6b79ad01fa284b8868f63f4a62a35817762dc982
Palak Shivlani <58842198+palakshivlani-11@users.noreply.github.com>
2022-03-02 01:40:45 +0530

    Update settings.py

--- personalportfolio/settings.py
+++ personalportfolio/settings.py
@@ -120,6 +120,9 @@ USE_L10N = True
 USE_TZ = True
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
