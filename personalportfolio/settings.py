94912aa977f34760e27d9319950a372c180eb061
Palak Shivlani <58842198+palakshivlani-11@users.noreply.github.com>
2022-04-03 09:10:59 +0530

    Update settings.py

--- personalportfolio/settings.py
+++ personalportfolio/settings.py
@@ -146,6 +146,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'shivlanipalak@@gmail.com'
-EMAIL_HOST_PASSWORD = 'chocoholic@2001'
+EMAIL_HOST_PASSWORD = 'danceholic@11'
 
 django_heroku.settings(locals())
